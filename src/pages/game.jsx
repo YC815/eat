8f7ab66d28d419a5668307296dd99f7847d2b908
YC815 <minnecraft.yc815@gmail.com>
2023-05-13 16:36:40 +0800

    遊戲頁面

---
+++ src/pages/game.jsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Image from 'next/image';
+import { Eater, Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import Link from 'next/link';
+
+export default function Home() {
+    return (
+        <div className="m-3 flex items-center flex-col">
+        </div>
+    );
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,10 +2,11 @@ import Head from "next/head";
 import Image from 'next/image';
 import { Eater, Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="m-3">
+    <div className="m-3 flex items-center flex-col">
       <h1 className="text-5xl font-a text-center ">吃！</h1>
 
       <div className="flex items-center justify-center flex-col m-9 ">
