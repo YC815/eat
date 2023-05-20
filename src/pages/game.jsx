4659a50aa1b1f2959eb799f2408b77cb9c8ffa5a
YC815 <minnecraft.yc815@gmail.com>
2023-05-20 17:06:09 +0800

    slic

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,47 +1,77 @@
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
-import Head from "next/head";
+import Head from 'next/head';
 import Image from 'next/image';
-import { Eater, Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
+import { Eater, Inter } from '@next/font/google';
+import styles from '@/styles/Home.module.css';
 import Link from 'next/link';
-
-const foodimage = [
-    '/food/1.jpg',
-    '/food/2.jpg'
-];
 
 export default function Home() {
     const router = useRouter();
     const [buttonClicked, setButtonClicked] = useState(false);
     const [randomImage, setRandomImage] = useState(null);
+    const [foodData, setFoodData] = useState(null);
 
     useEffect(() => {
-        console.log("頁面被載入");
+        console.log('頁面被載入');
 
-        const randomIndex = Math.floor(Math.random() * foodimage.length);
-        const randomImagePath = foodimage[randomIndex];
-        setRandomImage(randomImagePath);
-        console.log("圖片I");
+        const fetchFoodData = async () => {
+            try {
+                const response = await fetch('/food/food.json');
+                const data = await response.json();
+                setFoodData(data);
+            } catch (error) {
+                console.error('無法獲取食物數據：', error);
+            }
+        };
+
+        fetchFoodData();
     }, []);
 
-    {
-        randomImage && (
-            <div className={styles.imageContainer}>
-                <Image src={randomImage} alt="隨機圖片" width={300} height={200} />
-            </div>
-        )
-    }
+    useEffect(() => {
+        if (foodData) {
+            const randomFoodNumber =
+                Math.floor(Math.random() * Object.keys(foodData).length) + 1;
+            const foodList = foodData[randomFoodNumber];
+            const randomFoodIndex = Math.floor(Math.random() * foodList.length);
+            const randomFood = foodList[randomFoodIndex];
+
+            setRandomImage(`/food/${randomFoodNumber}.jpg`);
+        }
+    }, [foodData]);
 
     const eat = () => {
         console.log('玩家按下按鈕"吃"');
         setButtonClicked(true);
+
+        if (foodData && randomImage) {
+            const foodDiePoint = foodData[randomImage.slice(6, 7)][0].die_point;
+            const buffDiePoints = foodData[randomImage.slice(6, 7)][0].buff.map(
+                (buff) => buff[Object.keys(buff)[0]][0].die_point
+            );
+            const finalDiePoint = foodDiePoint + buffDiePoints.reduce((a, b) => a + b, 0);
+
+            const randomNum = Math.floor(Math.random() * 100);
+
+            if (randomNum < finalDiePoint) {
+                router.push('/die');
+            } else {
+                deat();
+            }
+        }
     };
 
     const deat = () => {
         console.log('玩家按下按鈕"不吃"');
-        setButtonClicked(true);
-        window.location.reload(); // 在按下"不吃"按钮后重新加载页面
+        if (foodData) {
+            const randomFoodNumber =
+                Math.floor(Math.random() * Object.keys(foodData).length) + 1;
+            const foodList = foodData[randomFoodNumber];
+            const randomFoodIndex = Math.floor(Math.random() * foodList.length);
+            const randomFood = foodList[randomFoodIndex];
+
+            setRandomImage(`/food/image/${randomFoodNumber}.jpg`);
+        }
     };
 
     return (
@@ -52,21 +82,42 @@ export default function Home() {
                         <Image src={randomImage} alt="隨機圖片" width={300} height={200} />
                     </div>
                 )}
-
-
+                {foodData && randomImage && (
+                    <div className="text-center mt-4">
+                        <h2 className="text-lg font-bold">
+                            {/* ex */}
+                            {foodData[randomImage.match(/\/([^/]+)\.[^.]+$/)[1]][0].name}
+                        </h2>
+                        <p>{foodData[randomImage.slice(6, 7)][0].description}</p>
+                        <p>死亡機率: {foodData[randomImage.slice(6, 7)][0].die_point}%</p>
+                        {foodData[randomImage.slice(6, 7)][0].buff.map((buff, index) => (
+                            <div key={index} className="mt-2">
+                                <h4 className="text-sm font-semibold">＿＿＿＿＿＿＿</h4>
+                                <p>{buff[Object.keys(buff)[0]][0].name}</p>
+                                <p>{buff[Object.keys(buff)[0]][0].description}</p>
+                                <p>死亡機率: {buff[Object.keys(buff)[0]][0].die_point}%</p>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
 
             <div className="absolute bottom-3 w-full">
                 <div className="flex justify-center">
-                    <button onClick={eat} className="rounded-full border mr-3 border-white p-3 text-5xl self-center bg-green-700 text-white">
+                    <button
+                        onClick={eat}
+                        className="rounded-full border mr-3 border-white p-3 text-5xl self-center bg-green-700 text-white"
+                    >
                         吃！
                     </button>
-                    <button onClick={deat} className="rounded-full border border-white p-3 text-5xl self-center bg-red-500 text-white">
+                    <button
+                        onClick={deat}
+                        className="rounded-full border border-white p-3 text-5xl self-center bg-red-500 text-white"
+                    >
                         不吃
                     </button>
                 </div>
             </div>
         </>
-
     );
 }
