f43444676c4b446353aad2774a3ed1e6a8136313
YC815 <minnecraft.YC815@gmail.com>
2023-05-20 22:46:33 +0800

    可遊玩

--- src/pages/die.jsx
+++ src/pages/die.jsx
@@ -1,18 +1,61 @@
-import Head from "next/head";
+// src/pages/die.jsx
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
-import { Eater, Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
 import Link from 'next/link';
 
-export default function Home() {
-    return (
-        <div className="m-3 flex items-center flex-col">
-            <Image src="/die.gif" alt="" height={1000} width={1000} />
-            <Link href='/'>
-                <button className="rounded-full border border-white p-3  text-5xl self-center m-3">
-                    重新
-                </button>
-            </Link>
-        </div>
-    );
+import styles from '@/styles/Home.module.css';
+
+export default function Die() {
+  const router = useRouter();
+  const [foodName, setFoodName] = useState(null);
+  const [buffDiePoints, setBuffDiePoints] = useState([]);
+  const [foodCounter, setFoodCounter] = useState(0);
+
+  useEffect(() => {
+    const lastFood = localStorage.getItem('lastFood');
+    if (lastFood) {
+      const [foodNumber, buffIndex] = lastFood.split('_');
+      const food = foodData[foodNumber][0];
+      setFoodName(food.name);
+      if (buffIndex && food.buff[buffIndex]) {
+        const buffDiePoint = food.buff[buffIndex][Object.keys(food.buff[buffIndex])[0]][0];
+        setBuffDiePoints([buffDiePoint]);
+      } else {
+        setBuffDiePoints([]);
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    const foodCounter = localStorage.getItem('foodCounter');
+    if (foodCounter) {
+      setFoodCounter(Number(foodCounter));
+    }
+  }, []);
+
+  return (
+    <div className="m-3 flex items-center flex-col">
+      <Image src="/die.gif" alt="" height={1000} width={1000} />
+      <div className="text-white text-xl mt-2 text-center">
+        已吃食物數量: {foodCounter}
+      {/* </div>
+      <div className="text-white text-xl mt-4 text-center">
+        <h2 className="text-2xl font-bold">死因：</h2>
+        {foodName && (
+          <ul className="list-disc list-inside">
+            <li>食物死因: {foodName}</li>
+            {buffDiePoints.map((diePoint, index) => (
+              <li key={index}>Buff死因: {diePoint?.name}</li>
+            ))}
+          </ul>
+        )}*/}
+      </div> 
+      <Link href="/">
+        <button className="rounded-full border border-white p-3  text-5xl self-center m-3">
+          重新
+        </button>
+      </Link>
+    </div>
+  );
 }
--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -2,122 +2,165 @@ import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Eater, Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
-import Link from 'next/link';
 
 export default function Home() {
-    const router = useRouter();
-    const [buttonClicked, setButtonClicked] = useState(false);
-    const [randomImage, setRandomImage] = useState(null);
-    const [foodData, setFoodData] = useState(null);
+  const router = useRouter();
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [randomFoodNumber, setRandomFoodNumber] = useState(null);
+  const [randomFoodIndex, setRandomFoodIndex] = useState(null);
+  const [randomFood, setRandomFood] = useState(null);
+  const [foodData, setFoodData] = useState(null);
+  const [prevRandomFood, setPrevRandomFood] = useState(null);
+  const [foodCounter, setFoodCounter] = useState(0); // 新增食物计数器
 
-    useEffect(() => {
-        console.log('頁面被載入');
-
-        const fetchFoodData = async () => {
-            try {
-                const response = await fetch('/food/food.json');
-                const data = await response.json();
-                setFoodData(data);
-            } catch (error) {
-                console.error('無法獲取食物數據：', error);
-            }
-        };
-
-        fetchFoodData();
-    }, []);
-
-    useEffect(() => {
-        if (foodData) {
-            const randomFoodNumber =
-                Math.floor(Math.random() * Object.keys(foodData).length) + 1;
-            const foodList = foodData[randomFoodNumber];
-            const randomFoodIndex = Math.floor(Math.random() * foodList.length);
-            const randomFood = foodList[randomFoodIndex];
-
-            setRandomImage(`/food/${randomFoodNumber}.jpg`);
+  useEffect(() => {
+    const fetchFoodData = async () => {
+      try {
+        const response = await fetch('/food/food.json');
+        if (!response.ok) {
+          throw new Error('请求食物数据失败');
         }
-    }, [foodData]);
-
-    const eat = () => {
-        console.log('玩家按下按鈕"吃"');
-        setButtonClicked(true);
-
-        if (foodData && randomImage) {
-            const foodDiePoint = foodData[randomImage.slice(6, 7)][0].die_point;
-            const buffDiePoints = foodData[randomImage.slice(6, 7)][0].buff.map(
-                (buff) => buff[Object.keys(buff)[0]][0].die_point
-            );
-            const finalDiePoint = foodDiePoint + buffDiePoints.reduce((a, b) => a + b, 0);
-
-            const randomNum = Math.floor(Math.random() * 100);
-
-            if (randomNum < finalDiePoint) {
-                router.push('/die');
-            } else {
-                deat();
-            }
-        }
+        const data = await response.json();
+        setFoodData(data);
+        console.log('食物种类数量：', Object.keys(data).length);
+      } catch (error) {
+        console.error('无法获取食物数据：', error);
+      }
     };
 
-    const deat = () => {
-        console.log('玩家按下按鈕"不吃"');
-        if (foodData) {
-            const randomFoodNumber =
-                Math.floor(Math.random() * Object.keys(foodData).length) + 1;
-            const foodList = foodData[randomFoodNumber];
-            const randomFoodIndex = Math.floor(Math.random() * foodList.length);
-            const randomFood = foodList[randomFoodIndex];
+    fetchFoodData();
+  }, []);
 
-            setRandomImage(`/food/image/${randomFoodNumber}.jpg`);
+  useEffect(() => {
+    if (foodData) {
+      const foodCategories = Object.keys(foodData);
+      const randomFoodNumber =
+        Math.floor(Math.random() * foodCategories.length) + 1;
+      const foodList = foodData[foodCategories[randomFoodNumber - 1]];
+      if (foodList) {
+        const randomFoodIndex = Math.floor(Math.random() * foodList.length);
+        const randomFood = foodList[randomFoodIndex];
+
+        setRandomFoodNumber(randomFoodNumber);
+        setRandomFoodIndex(randomFoodIndex);
+        setRandomFood(randomFood);
+      }
+    }
+  }, [foodData]);
+
+  const eat = () => {
+    console.log('玩家按下按鈕"吃"');
+    setButtonClicked(true);
+
+    if (randomFood) {
+      const foodDiePoint = randomFood.die_point;
+      const buffDiePoints = randomFood.buff.map(
+        (buff) => buff[Object.keys(buff)[0]][0].die_point
+      );
+      const finalDiePoint = foodDiePoint + buffDiePoints.reduce((a, b) => a + b, 0);
+
+      const randomNum = Math.floor(Math.random() * 100);
+
+      if (randomNum < finalDiePoint) {
+        router.push('/die');
+      } else {
+        setFoodCounter(foodCounter + 1); // 食物计数器加1
+        deat();
+      }
+    }
+  };
+
+  const deat = () => {
+    console.log('玩家按下按鈕"不吃"');
+    if (foodData) {
+      const foodCategories = Object.keys(foodData);
+      const remainingFoodCategories = foodCategories.filter(
+        (category) => category !== randomFoodNumber.toString()
+      );
+
+      if (remainingFoodCategories.length > 0) {
+        const randomFoodCategory =
+          remainingFoodCategories[
+            Math.floor(Math.random() * remainingFoodCategories.length)
+          ];
+        const foodList = foodData[randomFoodCategory];
+
+        if (foodList) {
+          const remainingFood = foodList.filter(
+            (food) =>
+              food !== randomFood && food !== prevRandomFood
+          );
+
+          if (remainingFood.length > 0) {
+            const newRandomFoodIndex = Math.floor(Math.random() * remainingFood.length);
+            const newRandomFood = remainingFood[newRandomFoodIndex];
+
+            setPrevRandomFood(randomFood);
+            setRandomFoodNumber(Number(randomFoodCategory));
+            setRandomFoodIndex(newRandomFoodIndex);
+            setRandomFood(newRandomFood);
+          }
         }
-    };
+      }
+    }
+  };
 
-    return (
-        <>
-            <div className="m-3 flex items-center flex-col">
-                {randomImage && (
-                    <div className={styles.imageContainer}>
-                        <Image src={randomImage} alt="隨機圖片" width={300} height={200} />
-                    </div>
-                )}
-                {foodData && randomImage && (
-                    <div className="text-center mt-4">
-                        <h2 className="text-lg font-bold">
-                            {/* ex */}
-                            {foodData[randomImage.match(/\/([^/]+)\.[^.]+$/)[1]][0].name}
-                        </h2>
-                        <p>{foodData[randomImage.slice(6, 7)][0].description}</p>
-                        <p>死亡機率: {foodData[randomImage.slice(6, 7)][0].die_point}%</p>
-                        {foodData[randomImage.slice(6, 7)][0].buff.map((buff, index) => (
-                            <div key={index} className="mt-2">
-                                <h4 className="text-sm font-semibold">＿＿＿＿＿＿＿</h4>
-                                <p>{buff[Object.keys(buff)[0]][0].name}</p>
-                                <p>{buff[Object.keys(buff)[0]][0].description}</p>
-                                <p>死亡機率: {buff[Object.keys(buff)[0]][0].die_point}%</p>
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div>
+  useEffect(() => {
+    if (foodCounter > 0) {
+      localStorage.setItem('foodCounter', foodCounter.toString());
+    }
+  }, [foodCounter]);
 
-            <div className="absolute bottom-3 w-full">
-                <div className="flex justify-center">
-                    <button
-                        onClick={eat}
-                        className="rounded-full border mr-3 border-white p-3 text-5xl self-center bg-green-700 text-white"
-                    >
-                        吃！
-                    </button>
-                    <button
-                        onClick={deat}
-                        className="rounded-full border border-white p-3 text-5xl self-center bg-red-500 text-white"
-                    >
-                        不吃
-                    </button>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="m-3 flex items-center flex-col">
+        {randomFood && (
+          <div className={styles.imageContainer}>
+            <Image
+              src={`/food/image/${randomFoodNumber}.jpg`}
+              alt="隨機圖片"
+              width={300}
+              height={200}
+            />
+          </div>
+        )}
+        {randomFood && (
+          <div className="text-center mt-4">
+            <h2 className="text-lg font-bold">{randomFood.name}</h2>
+            <p>{randomFood.description}</p>
+            <p>死亡機率: {randomFood.die_point}%</p>
+            {randomFood.buff.map((buff, index) => (
+              <div key={index} className="mt-2">
+                <h4 className="text-sm font-semibold">＿＿＿＿＿＿＿</h4>
+                <p>{buff[Object.keys(buff)[0]][0].name}</p>
+                <p>{buff[Object.keys(buff)[0]][0].description}</p>
+                <p>死亡機率: {buff[Object.keys(buff)[0]][0].die_point}%</p>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+
+      <div className="absolute bottom-3 w-full">
+        <div className="flex justify-center">
+          <button
+            onClick={eat}
+            className="rounded-full border mr-3 border-white p-3 text-5xl self-center bg-green-700 text-white"
+          >
+            吃！
+          </button>
+          <button
+            onClick={deat}
+            className="rounded-full border border-white p-3 text-5xl self-center bg-red-500 text-white"
+          >
+            不吃
+          </button>
+        </div>
+        <div className="text-white text-xl mt-2 text-center">
+          以吃食物適量: {foodCounter}
+        </div>
+      </div>
+    </>
+  );
 }
